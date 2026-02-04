135f22388f9fd98778afa108591da4fa41b42851
Lucas Eduardo de Melo <136135204+lucaseduardo5855@users.noreply.github.com>
2026-02-04 16:40:40 -0300

    HomePage

--- donuts-app/src/Components/Home.js
+++ donuts-app/src/Components/Home.js
@@ -1,6 +1,9 @@
 import React from "react"
 import NavBar from "./Navbar";
 import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from '../Assets/home-banner-image.png';
+import { FiArrowRight} from 'react-icons/fi';
+
 
  const Home = () => {
     return(
@@ -8,8 +11,29 @@ import BannerBackground from "../Assets/home-banner-background.png";
             <NavBar />        
 
             <div className="home-banner-container">
+                
                 <div className="home-bannerImage-container">
                     <img src={BannerBackground} alt="imagem de fundo da home container" />
+                </div>
+
+                <div className="home-text-section">
+                    <h1 className="primary-heading">
+                       Seu Donut Favorito Entregue em Sua Casa     
+                    </h1>
+                    <p className="primary-text">
+                        Diversos sabores deliciosos pra adoçar seus melhores momentos. Peça já o seu e receba um mimo de surpresa.
+                    </p>
+                    <button className="secundary-button">Faça seu pedido <FiArrowRight/></button>
+
+                    <p className="primary-text">
+                        Ou venha comer conosco em um ambiente climatizado e aconchegante com várias opções para seu pedido
+                    </p>
+                </div>
+
+                <div className="home-image-section">
+                    <div className="home-primary-image">
+                        <img src={BannerImage} alt="donuts" />
+                    </div>
                 </div>
             </div>
         </div>
