a65b88f26124d9967d874db2384aac1cb4e56d94
Lucas Eduardo de Melo <136135204+lucaseduardo5855@users.noreply.github.com>
2026-02-04 13:21:11 -0300

    consertando a pasta do projeto

---
+++ donuts-app/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import NavBar from "./Navbar";
+
+ const Home = () => {
+    return(
+        <div className="home-container">
+            <NavBar />        
+
+        </div>
+    );
+};
+
+
+export default  Home;
---
+++ donuts-app/src/Components/Navbar.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import Logo from "../Assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+
+
+// 1. AQUI ESTÁ O SEGREDO (Estrutura vem de @mui/material)
+
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import HomeWorkIcon from "@mui/icons-material/HomeWork";
+import List from "@mui/material/List";              
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+
+// 2. ÍCONES (Desenhos vem de @mui/icons-material)
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+
+const NavBar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "Sobre Nós",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Nosso Trabalho",
+      icon: <HomeWorkIcon />,
+    },
+    {
+      text: "Avaliação",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+        text: "Contato",
+        icon: <PhoneRoundedIcon/>
+    },
+    {
+      text: "Carrinho",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
+
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="logo donuts" />
+      </div>
+
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">Sobre Nós</a>
+        <a href="">Nossos Trabalhos</a>
+        <a href="">Avaliação</a>
+        <a href="">Contato</a>
+        <a href="">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+        <button className="primary-button">Reservar Agora</button>
+      </div>
+
+     {/* Menu de Hamburuger */}
+      <div className="nav-bar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+            sx={{width: 250}}
+            role="presentation"
+            onClick={() => setOpenMenu(false)}
+            onKeyDown={() => setOpenMenu(false)}
+        >
+         <List>
+            {menuOptions.map((item) => (
+                <ListItem key={item.text} disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText>{item.text}</ListItemText>
+                    </ListItemButton>
+                </ListItem>
+            ))}
+         </List>
+        </Box>
+      </Drawer>
+    </nav>
+  );
+};
+
+export default NavBar;
