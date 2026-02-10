45e7f2d609366b97e7e25d0365ce0c6ad25adff9
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-02-10 20:24:16 -0300

    Iniciando Contact

--- donuts-app/src/Components/Contact.js
+++ donuts-app/src/Components/Contact.js
@@ -1,7 +1,15 @@
 const Contact = () => {
   return (
-    <div>
-      <h1></h1>
+    <div className="contact-page-wrapper">
+      <p className="primary-subheading">Contacte-nos</p>
+      <h1 className="primary-heading">
+        Tem uma pergunta em mente? <br /> Deixe-nos ajudá-los
+      </h1>
+
+      <div className="contact-form-container">
+        <input type="text" name="" id="" placeholder="E-mail" />
+        <button className="secundary-button">Enviar</button>
+      </div>
     </div>
   );
 };
